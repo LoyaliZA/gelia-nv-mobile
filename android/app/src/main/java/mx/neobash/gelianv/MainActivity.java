89d8e5e1089c7e629a4b645fe71e86b2348ee5dd
Gabriel <contacto.loyal1a@gmail.com>
2026-09-18 09:08:38 -0600

    Despliegue del proyecto

---
+++ android/app/src/main/java/mx/neobash/gelianv/MainActivity.java
@@ -0,0 +1,5 @@
+package mx.neobash.gelianv;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'mx.neobash.gelianv',
+  appName: 'gelia-nv',
+  webDir: 'dist'
+};
+
+export default config;
